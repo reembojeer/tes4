0f6ae1a16ef7c96e0f8d4e280b65351e2c24ebd2
reembojeer <reembojeer@gmail.com>
2023-10-05 12:27:27 +0300

    Initial Commit

---
+++ tes4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tes4
+//
+//  Created by RIM ABDULLAH BUJEIR on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tes4/tes4App.swift
@@ -0,0 +1,17 @@
+//
+//  tes4App.swift
+//  tes4
+//
+//  Created by RIM ABDULLAH BUJEIR on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct tes4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
